8e002f2ba3df107de9350a142fa3ac2b16f4d5bf
Arindra Kumar Das <arindra.das@live.com>
2014-02-10 16:58:52 +0100

    Merge and conflicts

--- src/se/kth/csc/iprog/dinnerplanner/android/view/MenuView.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/view/MenuView.java
@@ -9,32 +9,6 @@ import android.widget.TextView;
 
 public class MenuView {
 
-	View view;
-	DinnerModel dm;
-
-	public MenuView() {
-		dm = new DinnerModel();
-
-		// store in the class the reference to the Android View
-		this.view = view.findViewById(R.id.menu_view);
-
-		NumberPicker np = (NumberPicker) view.findViewById(R.id.numberPicker1);
-		np.setMaxValue(9);
-		np.setMinValue(0);
-        np.setValue(dm.numberOfGuests);
-        np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-			
-			@Override
-			public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-				if(picker.getMaxValue() >= newVal && picker.getMinValue() <= newVal) {
-					picker.setValue(newVal);
-					dm.numberOfGuests = newVal;
-				}
-			}
-		});
-		
-		
-		// Setup the rest of the view layout
-	}
+	
 
 }
