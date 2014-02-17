566c8970fc645bf37f8bac1339217f5b2a2cd833
irene <paspatoufi@gmail.com>
2014-02-17 16:19:47 +0100

    some changes

--- src/se/kth/csc/iprog/dinnerplanner/android/view/SummaryViewController.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/view/SummaryViewController.java
@@ -12,6 +12,7 @@ import se.kth.csc.iprog.dinnerplanner.android.DinnerPlannerApplication;
 import se.kth.csc.iprog.dinnerplanner.android.R;
 import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
 import se.kth.csc.iprog.dinnerplanner.model.Dish;
+import se.kth.csc.iprog.dinnerplanner.model.Ingredient;
 
 public class SummaryViewController implements OnClickListener {
 
@@ -26,23 +27,37 @@ public class SummaryViewController implements OnClickListener {
 		findViewById(R.id.selected_main).setOnClickListener(this);
 		findViewById(R.id.selected_dessert).setOnClickListener(this);
 
+		StringBuilder sb = new StringBuilder();
+		for(Ingredient i : model.getAllIngredients()) {
+    		sb.append(i.getName()).append("    ").append(i.getQuantity()).append(" ").append(i.getUnit()).append("\n");
+    	}
 	}
 	@Override
 	public void onClick(View v) {
 		// TODO Put listeners for the imageViews or the RelativeLayout containing dish image/names
+		System.err.println("clicked");
+		if (v == findViewById(R.id.selected_instructions)){
+			TextView ingredients = (TextView) findViewById(R.id.selected_instructions);
+			ingredients.setText(sb.toString());
+		}
 		if (v == findViewById(R.id.selected_starter)){
 			TextView description = (TextView) findViewById(R.id.instructions);
+			description.setText(sb.toString());
 		}
 		if (v == findViewById(R.id.selected_main)){
 			TextView description = (TextView) findViewById(R.id.instructions);
+			description.setText(sb.toString());
 		}
 		if (v == findViewById(R.id.selected_dessert)){
-			Dish.getDescription();
+			TextView description = (TextView) findViewById(R.id.instructions);
+			description.setText(sb.toString());
 		}
 	}
 	
 	private View findViewById(int id) {
 		return view.view.findViewById(id);
 	}
+	
+
 
 }
