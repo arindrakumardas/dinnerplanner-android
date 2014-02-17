e280f331b675c6caca262432e8dd7f3c4c65041f
maggud <magnus.gudmandsen@gmail.com>
2014-02-17 16:08:16 +0100

    Implemented the MenuViewController to work properly with popups.

--- src/se/kth/csc/iprog/dinnerplanner/android/view/MenuViewController.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/view/MenuViewController.java
@@ -1,9 +1,14 @@
 package se.kth.csc.iprog.dinnerplanner.android.view;
 
+import android.app.Dialog;
+import android.graphics.Color;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemSelectedListener;
@@ -44,13 +49,113 @@ public class MenuViewController implements OnClickListener {
 		LinearLayout starters = (LinearLayout) findViewById(R.id.starters);
 		LinearLayout mains = (LinearLayout) findViewById(R.id.mains);
 		LinearLayout desserts = (LinearLayout) findViewById(R.id.desserts);
-		
+
+		addListenersToDishes(starters);
+		addListenersToDishes(mains);
+		addListenersToDishes(desserts);
 		
 	}
+	
 	@Override
 	public void onClick(View v) {
 		// TODO Put listeners for the imageViews or the RelativeLayout containing dish image/names
-		
+		if(v instanceof RelativeLayout) {
+			RelativeLayout rl = (RelativeLayout) v;
+			Dialog dialog = createDialog(rl);
+			if(dialog != null) {
+				dialog.show();
+			}
+		}
+	}
+	
+	private Dialog createDialog(RelativeLayout rl) {
+		for(int j = 0; j < rl.getChildCount(); j++) {
+			View currentView = rl.getChildAt(j);
+			if(currentView instanceof ImageView) {
+				ImageView iv = (ImageView) currentView;
+				final Dialog dialog = new Dialog(view.view.getContext());
+				dialog.setContentView(R.layout.popup);
+				//Set the image to the image of the clicked dish.
+				ImageView image = (ImageView) dialog.findViewById(R.id.popupImage);
+				image.setImageDrawable(iv.getDrawable());
+				//Set the title to the name of the clicked dish.
+				TextView name = (TextView) dialog.findViewById(R.id.popupName);
+				name.setText((String) rl.getTag()); //The tag will be a String.
+				//Set the cost to the cost of the clicked dish.
+				TextView cost = (TextView) dialog.findViewById(R.id.popupCost);
+				
+				//Find our dish
+				Dish currentDish = null;
+				//We set this tag when creating the RelativeLayout that contains a meal.
+				String currentString = (String) rl.getTag();
+				for(Dish dish : model.getDishes()) {
+					if(dish.getName().equals(currentString)) {
+						currentDish = dish;
+						cost.setText("Cost: " + currentDish.getCost()*model.getNumberOfGuests() + " kr\n(" + currentDish.getCost() + ") kr per person");
+						break;
+					}
+				}
+				final Dish dish = currentDish; //Should always be initialized.
+				
+				//Add listener to the Choose button
+				Button chooseBtn = (Button) dialog.findViewById(R.id.popup_choosebutton);
+				chooseBtn.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(View arg0) {
+						model.selectDish(dish);
+						dialog.dismiss();
+					}
+					
+				});
+				
+				//Add listener to the Cancel button
+				Button cancelBtn = (Button) dialog.findViewById(R.id.popup_cancelbutton);
+				cancelBtn.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(View arg0) {
+						dialog.dismiss();
+					}
+					
+				});
+				
+				return dialog;
+			}
+		}
+		return null; //Should never happen.
+	}
+	
+	/**
+	 * Adds listeners to the dishes contained in the LinearLayout
+	 */
+	private void addListenersToDishes(LinearLayout ll) {
+		for(int i = 0; i < ll.getChildCount(); i++) {
+			View currentChild = ll.getChildAt(i);
+			if(currentChild instanceof RelativeLayout) {
+				RelativeLayout currentLayout = (RelativeLayout) currentChild;
+				currentLayout.setClickable(true);
+				currentLayout.setOnClickListener(this);
+				//Object currentTag = currentLayout.getTag();
+				
+				for(int j = 0; j < currentLayout.getChildCount(); j++) {
+					View currentView = currentLayout.getChildAt(j);
+					if(currentView instanceof ImageView) {
+						ImageView currentImageView = (ImageView) currentView;
+						
+						break;
+					}
+				}
+				
+//				if(currentTag instanceof String) {
+//					//We set this tag when creating the RelativeLayout that contains a meal.
+//					String currentString = (String) currentTag;
+//					if(model.getSelectedDish(type).getName().equals(currentString)) {
+//						currentLayout.setBackgroundColor(Color.rgb(188, 13, 16)); //Should be the same color as the text on the start screen!
+//					}
+//				}
+			}
+		}
 	}
 	
 	private View findViewById(int id) {
