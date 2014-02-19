7ee84e674747a88c75a2c7a64c38aad282bd72bc
maggud <magnus.gudmandsen@gmail.com>
2014-02-19 11:13:58 +0100

    Fixed the null pointer exception in the summaryViewController

--- src/se/kth/csc/iprog/dinnerplanner/android/view/SummaryViewController.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/view/SummaryViewController.java
@@ -1,7 +1,9 @@
 package se.kth.csc.iprog.dinnerplanner.android.view;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewStub;
 import android.widget.TextView;
 import se.kth.csc.iprog.dinnerplanner.android.R;
 import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
@@ -29,8 +31,15 @@ public class SummaryViewController implements OnClickListener {
 	public void onClick(View v) {
 		// Put listeners for the imageViews or the RelativeLayout containing dish image/names
 		TextView description = (TextView) findViewById(R.id.selected_ingredients_summary);
+		TextView header = (TextView) findViewById(R.id.ing_sum);
+		//TextView header = (TextView) findViewById(R.id.ing_sum);
+		//LayoutInflater inflater = (LayoutInflater) this.view.view.getContext().getSystemService(this.view.view.getContext().LAYOUT_INFLATER_SERVICE);
+//		ViewStub stub = (ViewStub) findViewById(R.id.summary_stub);
+//		stub.setLayoutResource(R.layout.summary_ingredients);
+//		stub.inflate();
 		
 		if (v == findViewById(R.id.selected_instructions)){
+			header.setText("Ingredients");
 			StringBuilder sb = new StringBuilder();
 			for(Ingredient i : model.getAllIngredients()) {
 				sb.append(i.getName()).append("    ").append(i.getQuantity()).append(" ").append(i.getUnit()).append("\n");
@@ -39,18 +48,23 @@ public class SummaryViewController implements OnClickListener {
 		}
 
 		if (v == findViewById(R.id.selected_starter)){
-			description.setText("starter");
+
+			Dish dish = model.getSelectedDish(Dish.STARTER);
+			header.setText(dish.getName());
 			
-		/* The next line causes the crash: */
 			description.setText(dish.getDescription());
 		
 		}
 		if (v == findViewById(R.id.selected_main)){
-			description.setText("main");
+			Dish dish = model.getSelectedDish(Dish.MAIN);
+			description.setText(dish.getDescription());
+			header.setText(dish.getName());
 //			description.setText(dish.description);
 		}
 		if (v == findViewById(R.id.selected_dessert)){
-			description.setText("dessert");
+			Dish dish = model.getSelectedDish(Dish.DESERT);
+			header.setText(dish.getName());
+			description.setText(dish.getDescription());
 //			description.setText(dish.description);
 		}
 	}
